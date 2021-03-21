5269889ef7cf5bf4cc9dbae3be118612eaace336
mac <mac@macs-MacBook-Pro.local>
2021-03-21 19:35:32 +0530

    server file change

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/customer-view'));
+app.use(express.static(__dirname + '/dist/customerView'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/customer-view/index.html'));
+res.sendFile(path.join(__dirname+'/dist/customerView/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
